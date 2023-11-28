2614252b3d09f154b1ba6d8921e44db7dfabe378
CletsyMedia <Cletussam12@yahoo.com>
2023-11-28 13:10:03 +0100

    Not quite HBTN yet

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# Extract the first command line argument
+input_string = ARGV[0]
+
+# Define the regular expression to match a string starting with 'h', ending with 'n',
+# and having any single character in between
+regex_pattern = /^h.n$/
+
+# Use match method to find matches in the input string
+match_result = input_string.match(regex_pattern)
+
+# Print the matched portion
+puts match_result ? match_result[0]
