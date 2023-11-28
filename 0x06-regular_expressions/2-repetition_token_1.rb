6caa8d40478c91aabc2975752b07a15436f3f84a
CletsyMedia <Cletussam12@yahoo.com>
2023-11-28 12:54:41 +0100

    Repetition #2

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,13 +1,7 @@
 #!/usr/bin/env ruby
-
-# Extract the first command line argument
-input_string = ARGV[0]
-
-# Define the regular expression with a repetition token to match "hbt" with an optional 't'
-regex_pattern = /hbt?n/
-
-# Use the match method to find matches in the input string
-match_result = input_string.match(regex_pattern)
-
-# Print the matched portion
-puts match_result ? match_result[0] : "No match"
+# This Ruby script accepts a string as a command line argument and uses a regular expression
+# to match instances of "hbtn" with an optional 't'. The script then prints the matched portions
+# by joining the results. The regular expression uses the '?' quantifier to allow for zero or
+# one occurrence of the character 't' after 'b'. This concise script efficiently handles the
+# specified pattern, providing a clear and readable solution.
+puts ARGV[0].scan(/hb?tn/).join
