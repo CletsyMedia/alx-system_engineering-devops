ffb05112ab63a0bc08585311091d9970ff49a3d6
CletsyMedia <Cletussam12@yahoo.com>
2023-11-28 12:49:37 +0100

    Repetition #2

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -3,8 +3,8 @@
 # Extract the first command line argument
 input_string = ARGV[0]
 
-# Define the regular expression with a repetition token to match "hbt" with an optional 't'
-regex_pattern = /hbt{0,1}n/
+# Define the regular expression to match "hbt" with an optional 't'
+regex_pattern = /hbt?n/
 
 # Use scan method to find matches in the input string
 matches = input_string.scan(regex_pattern)
