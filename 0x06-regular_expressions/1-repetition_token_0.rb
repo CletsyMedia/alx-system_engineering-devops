0681126c20c16f9c6dc15bf1145e83d886df34c6
CletsyMedia <Cletussam12@yahoo.com>
2023-11-28 09:44:01 +0100

    Repetition Token

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# Extract the first command line argument
+input_string = ARGV[0]
+
+# Define the regular expression with a repetition token to match "hbt" with 2 to 5 occurrences
+regex_pattern = /hbt{2,5}n/
+
+# Use scan method to find matches in the input string
+matches = input_string.scan(regex_pattern)
+
+# Print the matches joined by a newline
+puts matches.join
