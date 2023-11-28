453bee99cca252e502b8b2a89a492f630883dd16
CletsyMedia <Cletussam12@yahoo.com>
2023-11-28 13:07:12 +0100

    Expression to match hbtn with 2 to 5 occurrences of 't' after 'b'

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# Extract the first command line argument
+input_string = ARGV[0]
+
+# Define the regular expression to match "hbtn" with 2 to 5 occurrences of 't' after 'b'
+regex_pattern = /hbt*t*n/
+
+# Use scan method to find matches in the input string
+matches = input_string.scan(regex_pattern)
+
+# Print the matches joined by a newline
+puts matches.join
