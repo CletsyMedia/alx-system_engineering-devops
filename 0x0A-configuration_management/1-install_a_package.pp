4a9e944aa161d9e1c966cf82c927bf6327a966b0
CletsyMedia <Cletussam12@yahoo.com>
2024-01-19 13:52:27 +0100

    Installing packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,4 +4,11 @@
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  install_options => ['--user'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  install_options => ['--user'],
 }
