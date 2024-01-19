e6484e3abc70b56d8d8e40dd5482091cc29d8473
CletsyMedia <Cletussam12@yahoo.com>
2024-01-19 13:54:02 +0100

    Installing packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,13 +2,13 @@
 # Puppet manifest to install Flask from pip3 with a specific version.
 
 package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure          => '2.1.0',
+  provider        => 'pip3',
   install_options => ['--user'],
 }
 
 package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
+  ensure          => '2.1.1',
+  provider        => 'pip3',
   install_options => ['--user'],
 }
