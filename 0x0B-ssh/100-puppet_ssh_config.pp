6bcabd7bc3e85fb737841509da3b0632fd487a8f
CletsyMedia <Cletussam12@yahoo.com>
2024-01-21 17:29:50 +0100

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# Setting up my .ssh/config files using puppet
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+}
