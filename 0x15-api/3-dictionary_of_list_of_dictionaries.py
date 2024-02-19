a1d0186ebb1ee4b3a0c95617bbead7a46c221d7c
CletsyMedia <Cletussam12@yahoo.com>
2024-02-19 16:59:31 +0100

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,25 +6,30 @@ import sys
 
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = '{}users'.format(url)
-    res = requests.get(user)
-    json_o = res.json()
-    d_task = {}
-    for user in json_o:
-        name = user.get('username')
-        userid = user.get('id')
-        todos = '{}todos?userId={}'.format(url, userid)
-        res = requests.get(todos)
-        tasks = res.json()
-        l_task = []
-        for task in tasks:
-            dict_task = {"username": name,
-                         "task": task.get('title'),
-                         "completed": task.get('completed')}
-            l_task.append(dict_task)
+    api_url = "https://jsonplaceholder.typicode.com/"
+    users_endpoint = "{}users".format(api_url)
+    users_response = requests.get(users_endpoint)
+    users_data = users_response.json()
+    tasks_dict = {}
 
-        d_task[str(userid)] = l_task
-    filename = 'todo_all_employees.json'
-    with open(filename, mode='w') as f:
-        json.dump(d_task, f)
+    for user_data in users_data:
+        username = user_data.get("username")
+        user_id = user_data.get("id")
+        todos_endpoint = "{}todos?userId={}".format(api_url, user_id)
+        todos_response = requests.get(todos_endpoint)
+        todos_data = todos_response.json()
+        user_tasks = []
+
+        for task_data in todos_data:
+            task_dict = {
+                "username": username,
+                "task": task_data.get("title"),
+                "completed": task_data.get("completed"),
+            }
+            user_tasks.append(task_dict)
+
+        tasks_dict[str(user_id)] = user_tasks
+
+    output_filename = "todo_all_employees.json"
+    with open(output_filename, mode="w") as output_file:
+        json.dump(tasks_dict, output_file)
