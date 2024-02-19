07959452b841449b38ab77f989e9dcd66fc5f7ef
CletsyMedia <Cletussam12@yahoo.com>
2024-02-19 16:09:06 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,30 +16,32 @@ def gather_employee_todo_progress(employee_id):
     """
 
     # Base URL of the REST API
-    base_url = 'https://jsonplaceholder.typicode.com/'
+    base_url = "https://jsonplaceholder.typicode.com/"
 
     # Fetch user info
-    user_response = requests.get(base_url + 'users/{}'.format(employee_id))
+    user_response = requests.get(base_url + "users/{}".format(employee_id))
     user_data = user_response.json()
-    employee_name = user_data.get('name')
+    employee_name = user_data.get("name")
 
     # Fetch TODO list
-    todo_response = requests.get(base_url + 'todos',
-                                 params={'userId': employee_id})
+    todo_response = requests.get(base_url + "todos", params={"userId": employee_id})
     todo_data = todo_response.json()
 
     # Filter completed tasks
-    completed_tasks = [task for task in todo_data if task.get('completed')]
+    completed_tasks = [task for task in todo_data if task.get("completed")]
 
     # Display progress
     total_tasks = len(todo_data)
     completed_count = len(completed_tasks)
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, completed_count, total_tasks))
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name, completed_count, total_tasks
+        )
+    )
 
     for task in completed_tasks:
-        print("\t{}".format(task.get('title')))
+        print("\t{}".format(task.get("title")))
 
 
 if __name__ == "__main__":
