a15fcc6fd5cc67c4d2d30b4db37de7b945a45382
CletsyMedia <Cletussam12@yahoo.com>
2024-02-19 16:36:33 +0100

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-""" Script that exports data to CSV format """
+""" Script that uses JSONPlaceholder API to get information about employee """
 import csv
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
+    base_url = 'https://jsonplaceholder.typicode.com/'
 
     user_id = sys.argv[1]
-
-    # Fetch user details
-    user_response = requests.get(f"{url}users/{user_id}")
+    user_url = '{}users/{}'.format(base_url, user_id)
+    user_response = requests.get(user_url)
     user_data = user_response.json()
-    username = user_data.get("username")
+    username = user_data.get('username')
 
-    # Fetch tasks
-    tasks_response = requests.get(f"{url}todos?userId={user_id}")
-    tasks = tasks_response.json()
+    todos_url = '{}todos?userId={}'.format(base_url, user_id)
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+    tasks = []
+    for todo in todos_data:
+        tasks.append([user_id,
+                      username,
+                      todo.get('completed'),
+                      todo.get('title')])
 
-    # Prepare CSV file name
-    csv_filename = f"{user_id}.csv"
-
-    # Write tasks to CSV file
-    with open(csv_filename, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(
-            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+    filename = '{}.csv'.format(user_id)
+    with open(filename, mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file,
+                                delimiter=',',
+                                quotechar='"',
+                                quoting=csv.QUOTE_ALL)
         for task in tasks:
-            task_completed_status = "True" if task.get(
-                "completed") else "False"
-            task_title = task.get("title")
-            csv_writer.writerow(
-                [user_id, username, task_completed_status, task_title])
-
-    print(f"Data exported to {csv_filename} successfully.")
+            csv_writer.writerow(task)
