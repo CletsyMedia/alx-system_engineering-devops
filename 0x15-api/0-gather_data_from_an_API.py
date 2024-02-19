c901a29e38b5c6ca8a19d5ee6d76a29f4b9b80aa
CletsyMedia <Cletussam12@yahoo.com>
2024-02-19 16:07:25 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,7 @@ from a given REST API.
 """
 
 import requests
-from sys import argv
+import sys
 
 
 def gather_employee_todo_progress(employee_id):
@@ -16,41 +16,39 @@ def gather_employee_todo_progress(employee_id):
     """
 
     # Base URL of the REST API
-    base_url = "https://jsonplaceholder.typicode.com/"
+    base_url = 'https://jsonplaceholder.typicode.com/'
 
     # Fetch user info
-    user_response = requests.get(base_url + "users/{}".format(employee_id))
+    user_response = requests.get(base_url + 'users/{}'.format(employee_id))
     user_data = user_response.json()
-    employee_name = user_data.get("name")
+    employee_name = user_data.get('name')
 
     # Fetch TODO list
-    todo_response = requests.get(base_url + "todos", params={"userId": employee_id})
+    todo_response = requests.get(base_url + 'todos',
+                                 params={'userId': employee_id})
     todo_data = todo_response.json()
 
     # Filter completed tasks
-    completed_tasks = [task for task in todo_data if task.get("completed")]
+    completed_tasks = [task for task in todo_data if task.get('completed')]
 
     # Display progress
     total_tasks = len(todo_data)
     completed_count = len(completed_tasks)
 
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            employee_name, completed_count, total_tasks
-        )
-    )
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, completed_count, total_tasks))
 
     for task in completed_tasks:
-        print("\t{}".format(task.get("title")))
+        print("\t{}".format(task.get('title')))
 
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: {} <employee_id>".format(argv[0]))
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
         exit(1)
 
     try:
-        employee_id = int(argv[1])
+        employee_id = int(sys.argv[1])
     except ValueError:
         print("Employee ID must be an integer")
         exit(1)
