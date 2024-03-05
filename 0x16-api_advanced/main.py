4067d83f2980d7300532b36da790dd7022a0c7e2
CletsyMedia <Cletussam12@yahoo.com>
2024-03-05 22:25:30 +0100

    Top ten

--- 0x16-api_advanced/main.py
+++ 0x16-api_advanced/main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-0-main
+1-main
 """
 import sys
 
 if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    top_ten = __import__('1-top_ten').top_ten
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
+        top_ten(sys.argv[1])
