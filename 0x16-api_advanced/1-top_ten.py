bccda077c2c37533e1161b72767ed357e2b323a4
CletsyMedia <Cletussam12@yahoo.com>
2024-03-05 23:22:29 +0100

    1-top tens

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,40 +1,4 @@
 #!/usr/bin/python3
-"""
-A function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
-"""
-
-import requests
-
-def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to query.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json().get('data', {})
-        children = data.get('children', [])
-        if children:
-            for i, child in enumerate(children[:10], 1):
-                print(f"{i}. {child['data']['title']}")
-        else:
-            print("No hot posts found for this subreddit.")
-    else:
-        print("None")
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
-#!/usr/bin/python3
 
 """
 prints the titles of the first 10 hot posts listed for a given subreddit
