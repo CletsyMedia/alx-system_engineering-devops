56d59cf8e9cf9662dbea2a61b9dd54916a803fac
CletsyMedia <Cletussam12@yahoo.com>
2024-03-06 23:25:03 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,35 +1,13 @@
-# Puppet manifest to fix Apache 500 error
+# Puppet manifest to fix the issue identified with strace
 
-# Ensure Apache package is installed
-package { 'apache2':
-  ensure => installed,
+# Replace this with the actual fix for the issue
+exec { 'fix_apache_issue':
+  command => '/bin/echo "Fixing Apache issue"',
 }
 
-# Ensure Apache service is running
+# Notify a service restart if necessary
 service { 'apache2':
-  ensure => running,
+  ensure  => running,
+  enable  => true,
+  require => Exec['fix_apache_issue'],
 }
-
-# Define a file resource to fix the issue in Apache configuration
-file { '/etc/apache2/apache2.conf':
-  ensure  => file,
-  content => template('apache2/apache2.conf.erb'),
-  require => Package['apache2'],
-  notify  => Service['apache2'],
-}
-
-# Define the Apache virtual host configuration
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  content => template('apache2/000-default.conf.erb'),
-  require => Package['apache2'],
-  notify  => Service['apache2'],
-}
-
-# Restart Apache service after configuration changes
-exec { 'restart_apache':
-  command     => '/etc/init.d/apache2 restart',
-  refreshonly => true,
-  subscribe   => [File['/etc/apache2/apache2.conf'], File['/etc/apache2/sites-available/000-default.conf']],
-}
-
