35052f344c26da17c1845e0f821d330db5e354fc
CletsyMedia <Cletussam12@yahoo.com>
2024-03-07 00:06:51 +0100

    Update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,15 @@
 # Puppet manifest to fix the issue identified with strace
 
 # Replace bad "phpp" extension to "php" in "wp-settings.php".
-exec{ 'fix_wordpress':
+exec { 'fix_wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path 	  => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # Notify a service restart if necessary
 service { 'apache2':
   ensure  => running,
   enable  => true,
-  require => Exec['fix_apache_issue'],
+  require => Exec['fix_wordpress'],
 }
+
