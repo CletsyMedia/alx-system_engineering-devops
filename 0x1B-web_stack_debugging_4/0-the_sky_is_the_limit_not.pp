006f312321d4f327eeafe2c4f3a0a1b099b33a49
CletsyMedia <Cletussam12@yahoo.com>
2024-03-14 06:19:39 +0100

    Increase the ULIMIT

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,16 @@
 # Increasing the amount of traffic an Nginx server should handle
 
 # Increase the ULIMIT of the default file
+# fix request limit at nginx
+
 exec { 'fix--for-nginx':
-  # Modification of the ULIMIT value for Nginx server
-  command => '/bin/sed -i "s/ulimit -n 15/ulimit -n 4096/" /etc/default/nginx',
-  # Specificity for the path
-  path    => '/usr/local/bin/:/bin/',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-# Restarting Nginx server
-exec { 'restart-nginx':
-  # Restarting Nginx server
-  command => '/etc/init.d/nginx restart',
-  # Specificity for the path to initialize the script
-  path    => '/etc/init.d/',
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
+
