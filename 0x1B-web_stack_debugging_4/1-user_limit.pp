566788bda65433feccf7b7d47a5cf67234b24a45
CletsyMedia <Cletussam12@yahoo.com>
2024-03-14 06:35:13 +0100

    Updating OS configuration

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,17 +1,12 @@
 # Puppet manifest to change the OS configuration for the holberton user
 
-# Define the maximum number of file descriptors
-$desired_file_limit = 4096
-
 # Change the OS configuration for the holberton user
-exec { 'increase-hard-file-limits-for-holberton-user':
-  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/'
+exec {'increase_hard':
+command => '/bin/sed -i "s/holberton hard nofile 5/holberton hard nofile 65535/g" /etc/security/limits.conf',
 }
 
 # Increase the soft file limits for holberton-user
-exec { 'increase-soft-file-limits-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec {'increase_soft':
+command => '/bin/sed -i "s/holberton soft nofile 4/holberton soft nofile 65535/g" /etc/security/limits.conf',
 }
 
